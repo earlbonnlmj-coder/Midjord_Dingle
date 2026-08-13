ffb993b583b270023c2eee73bdf53c3db527a64e
Earl Bonn Midjord <earlbonnlmj@gmail.com>
2026-08-14 01:44:56 +0800

    Phase 2

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
@@ -17,6 +22,7 @@ import com.example.unscramble.ui.theme.UnscrambleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             UnscrambleTheme {
                 GameScreen()
@@ -27,35 +33,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen() {
-<<<<<<< HEAD
-=======
     var userAnswer by remember {
         mutableStateOf("")
     }
 
-    val words = listOf(
-        "CAT",
-        "DOG",
-        "BOOK"
-    )
-
-    var currentWordIndex by remember {
-        mutableStateOf(0)
-    }
-
-    val correctAnswer = words[currentWordIndex]
-
-    var scrambledWord by remember {
-        mutableStateOf(
-            words[0].shuffled().joinToString("")
-        )
-    }
-
-    var score by remember {
-        mutableStateOf(0)
-    }
-
->>>>>>> 2a8ad9c31e63d69c91b444e2c2f87c717f0a67bd
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -67,11 +48,7 @@ fun GameScreen() {
         )
 
         Text(
-<<<<<<< HEAD
             text = "TAC",
-=======
-            text = scrambledWord,
->>>>>>> 2a8ad9c31e63d69c91b444e2c2f87c717f0a67bd
             fontSize = 40.sp
         )
 
@@ -80,31 +57,17 @@ fun GameScreen() {
         )
 
         OutlinedTextField(
-            value = "",
-            onValueChange = { },
+            value = userAnswer,
+            onValueChange = {
+                userAnswer = it
+            },
             label = {
                 Text("Enter your answer")
             }
         )
 
         Button(
-<<<<<<< HEAD
             onClick = { }
-=======
-            onClick = {
-                if (userAnswer == correctAnswer) {
-                    score++
-
-                    if (currentWordIndex < words.size - 1) {
-                        currentWordIndex++
-                        userAnswer = ""
-                        scrambledWord = words[currentWordIndex]
-                            .shuffled()
-                            .joinToString("")
-                    }
-                }
-            }
->>>>>>> 2a8ad9c31e63d69c91b444e2c2f87c717f0a67bd
         ) {
             Text("SUBMIT")
         }
