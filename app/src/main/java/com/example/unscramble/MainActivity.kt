5b33d3da76d019af21801cb0f35ef53e1727a5eb
Earl Bonn Midjord <earlbonnlmj@gmail.com>
2026-09-07 02:31:31 +0800

    Phase 7 Add UI State and StateFlow

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
@@ -29,8 +31,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen() {
-    // Phase 6: Obtain instance of GameViewModel
     val viewModel: GameViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -41,29 +43,27 @@ fun GameScreen() {
             text = "UNSCRAMBLE",
             fontSize = 30.sp
         )
-        // Phase 6: Read word directly from ViewModel
         Text(
-            text = viewModel.words[viewModel.currentWordIndex],
+            text = uiState.scrambledWord,
             fontSize = 40.sp
         )
         Text(
             text = "Unscramble the word!"
         )
         OutlinedTextField(
-            value = viewModel.userAnswer,
-            onValueChange = { },
+            value = uiState.userAnswer,
+            onValueChange = {},
             label = {
                 Text("Enter your answer")
             }
         )
         Button(
-            onClick = { }
+            onClick = {}
         ) {
             Text("SUBMIT")
         }
-        // Phase 6: Read score directly from ViewModel
         Text(
-            text = "Score: ${viewModel.score}"
+            text = "Score : ${uiState.score}"
         )
     }
 }
